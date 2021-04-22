f0dae1c5a2b55783256fac6e2497e7465518731b
sky <alexandra.u1010@gmail.com>
2021-04-22 15:57:37 -0700

    first commit

---
+++ VaccTrack.py
@@ -0,0 +1,92 @@
+from flask import g, render_template, request, make_response, redirect, url_for, jsonify
+from setup import app, _PORT,_HOST, _APP_NAME, _PATIENTS_TABLE,_MEASUREMENTS_TABLE,_PLACEHOLDER
+from HIM73050 import flask_db as fdb
+from login_auth import login_required
+from flask import jsonify
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    fdb.close_db()
+
+@app.route('/index')
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+'''
+    Steps for porting v2 to v3, update routers and templates.
+
+    1) copy measurements(ptid) router code to a NEW patients() router
+    and modify its code so that it manages the patient list (allows listing, selecting patients, adding new patient, etc);
+        this is similar to how multiple measurements list was managed for the ONE patient in v2 app
+    2) rename the measurements(ptid) router to patient(ptid)
+        and modify its code so that it will displays information about ONE patient
+        and manages their measurement list;
+        this is similar to how information about one measurement was managed in v2 app
+        (i.e., list, select, add new measurement)
+    3) update the measurement(ptid,mid) router
+    and modify its code to allow for foth a patient info object and
+    a measurement info object be passed along to the templates
+
+    4) rename and modify the templates to reflect the above changes
+
+there is no need to pass the patient argument to the routers;
+there will be a patient ID and a patient_info object
+'''
+@app.route('/roster')
+@login_required
+def display_roster():
+    return render_template('roster.html')
+
+@app.route('/patient_select')
+@login_required
+def select_patient():
+    return render_template('patient_selector.html')
+
+@app.route('/patients',methods=['GET', 'POST'])
+@login_required
+def patients():
+    '''Works'''
+    if request.method == 'GET':
+        patient_list = fdb.query_db_get_all('SELECT * FROM '+_PATIENTS_TABLE)
+        print(patient_list)
+        return jsonify(patient_list)
+    elif request.method == 'POST':
+        fName = request.form['fname']
+        lName = request.form['lname']
+        result=fdb.query_db_change('INSERT INTO '+_PATIENTS_TABLE+'(firstName, lastName) VALUES ({0}, {0})'.format(_PLACEHOLDER),(fName, lName))
+        if result==None:
+            print("Could not insert new record into",_PATIENTS_TABLE)
+        return redirect(url_for('select_patient'))
+
+@app.route('/patient/<ptid>/'+_APP_NAME, methods=['GET', 'POST'])
+@login_required
+def patient(ptid):
+    '''Works '''
+    if request.method == 'GET':
+        patient_info = fdb.query_db_get_one('SELECT * FROM '+_PATIENTS_TABLE+' WHERE id={0}'.format(_PLACEHOLDER),(ptid,))
+        #retrieve measurements
+        query='SELECT * FROM '+_MEASUREMENTS_TABLE+' WHERE patient_id={0}'.format(_PLACEHOLDER) # this IS good enough@
+        #no need to join - keeping the patient and measurements dictionary separate, makes it easier/more intuitive to write the template code
+        #query='SELECT * FROM '+_MEASUREMENTS_TABLE+' as v JOIN (SELECT * FROM Patients WHERE id={0}) as p ON v.patient_id=p.id'.format(_PLACEHOLDER)
+        patient_measurements = fdb.query_db_get_all(query,(ptid,))
+        return render_template('patient.html', patient=patient_info, measurements=patient_measurements)
+
+    elif request.method == 'POST':
+        systolic = request.form['systolic']
+        diastolic = request.form['diastolic']
+        pulse = request.form['pulse']
+        fdb.query_db_change('INSERT INTO '+_MEASUREMENTS_TABLE+'(systolic, diastolic, pulse, patient_id) VALUES ({0}, {0}, {0}, {0})'.format(_PLACEHOLDER),(systolic, diastolic, pulse,ptid))
+        return redirect(url_for('patient',ptid=ptid))
+
+@app.route('/patient/<ptid>/'+_APP_NAME+'/measurement/<mid>')
+@login_required
+def measurement(ptid,mid):
+    patient_info = fdb.query_db_get_one('SELECT * FROM '+_PATIENTS_TABLE+' WHERE id={0}'.format(_PLACEHOLDER),(ptid,))
+    pt_msmt=fdb.query_db_get_one('SELECT id, systolic, diastolic, pulse, time_recorded FROM '+_MEASUREMENTS_TABLE+' WHERE id = {0}'.format(_PLACEHOLDER), (mid,))
+    return render_template('measurement.html',patient=patient_info, measurement=pt_msmt)
+
+if __name__ == '__main__':
+    app.run(port=_PORT, host=_HOST)
+
---
+++ database/vacc_track.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.13-MariaDB : Database - vacc_track
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`vacc_track` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `vacc_track`;
+
+/*Table structure for table `patients` */
+
+DROP TABLE IF EXISTS `patients`;
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `patients` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`) values
+(1,'admin','pbkdf2:sha256:150000$KlhH6cH0$48e1d503ed407a1b38f8de262bb21fe7665cb2a489c5b519abb89d4102a524f8');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
